e9f0c7bc7cf94955ff4112040fb0350029765e1e
scirexs <xer_xrs00@msn.com>
2024-11-25 03:59:14 +0000

    Modify value props of ColorPicker.

--- src/lib/comp/_ColorPicker.svelte
+++ src/lib/comp/_ColorPicker.svelte
@@ -2,9 +2,9 @@
   /*** Export ***/
   export type Props = {
     status?: State,  // bindable, [STATE.DEFAULT]
-    value?: string,  // bindable, ["#000000"]
+    value?: string,  // bindable, ["0 0 0"]
     alpha?: number,  // bindable, [1]
-    style?: DefineStateStyle | DefineStyle | StyleSet,
+    style?: DefineStateStyle | DefineStyle,
     action?: Action,
     events?: EventSet,
     attributes?: HTMLAttributes<HTMLInputElement>;
@@ -16,11 +16,41 @@
     PART.MAIN,
   ] as const;
 
+  export type RgbColor = [number, number, number];
+  export function getRgb(hex: string): RgbColor {
+    if (!hex.startsWith("#") || (hex.length !== 7 && hex.length !== 4)) { return [0, 0, 0]; }
+    if (hex.length === 4) {
+      const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, (_, r, g, b) => r+r + g+g + b+b);
+    }
+    const ret = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if (!ret) { return [0, 0, 0] }
+    ret.shift();
+    return ret.map(x => Number.parseInt(x, 16)) as RgbColor;
+  }
+  export function getHex(rgb: RgbColor): string {
+    if (!isRgbColor(rgb)) { return "#000000"; }
+    return "#" + (1 << 24 | rgb[0] << 16 | rgb[1] << 8 | rgb[2]).toString(16).slice(1);
+  }
+  export function castStringToRgbColor(str: string): RgbColor {
+    const ary = str.split(" ").map(x => Number.parseInt(x));
+    if (ary.length !== 3) { return [0,0,0]; }
+    if (!isRgbColor(ary as RgbColor)) { return [0,0,0]; }
+    return ary as RgbColor;
+  }
+
   /*** Others ***/
+  function isRgbColor(rgb: RgbColor): boolean {
+    for (let i=0; i<rgb.length; i++) {
+      if (!(rgb[i] >= 0 && rgb[i] < 256)) { return false; }
+    }
+    return true;
+  }
 
   /*** import ***/
   import { type Action } from "svelte/action";
   import { type HTMLAttributes } from "svelte/elements";
+  import { untrack } from "svelte";
   import { STATE, PART } from "$lib/const";
   import { getApplyStyle, omit } from "$lib/util";
   import { stdColorPicker } from "$lib/style";
@@ -28,15 +58,23 @@
 
 <!---------------------------------------->
 
+<!-- svelte-ignore state_referenced_locally -->
 <script lang="ts">
-  let { status = $bindable(STATE.DEFAULT), value = $bindable("#000000"), alpha = $bindable(1), style, action, events, attributes, element = $bindable() }: Props = $props();
+  let { status = $bindable(STATE.DEFAULT), value = $bindable("0 0 0"), alpha = $bindable(1), style, action, events, attributes, element = $bindable() }: Props = $props();
 
   /*** Initialize ***/
   const attr = omit({...attributes}, ["type", "disabled"]);
   let disabled = $derived(status === STATE.DISABLE);
-  let rgb = $derived(getRgb(value));
+  let rgb = $derived(castStringToRgbColor(value));
+  let hex = $state(getHex(rgb));
 
   /*** Sync with outside ***/
+  $effect.pre(() => { hex;
+    untrack(() => value = getRgb(hex).join(" "));
+  });
+  $effect(() => { value;
+    untrack(() => hex = getHex(rgb));
+  });
 
   /*** Styling ***/
   const myStyleSet = style === undefined ? stdColorPicker : stdColorPicker.toMerge(style);
@@ -47,18 +85,6 @@
   /*** Validation ***/
 
   /*** Others ***/
-  function getRgb(hex: string): number[] {
-    if (!hex.startsWith("#") || (hex.length !== 7 && hex.length !== 4)) { return [0, 0, 0]; }
-    if (hex.length === 4) {
-      const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, (_, r, g, b) => r+r + g+g + b+b);
-    }
-
-    const ret = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    if (!ret) { return [0, 0, 0] }
-    ret.shift();
-    return ret.map(x => Number.parseInt(x, 16));
-  }
 
   /*** Handle events ***/
 </script>
@@ -70,9 +96,9 @@
   <div style="display: inline-block; background-image: linear-gradient(45deg,#ccc 25%,transparent 25%),linear-gradient(-45deg,#ccc 25%,transparent 25%),linear-gradient(45deg,transparent 75%,#ccc 75%),linear-gradient(-45deg,transparent 75%,#ccc 75%); background-size: 20px 20px; background-position: 0 0,0 10px,10px -10px,-10px 0px;">
     <div class={myStyle[PART.MAIN]} style={`background-color: rgba(${rgb[0]},${rgb[1]},${rgb[2]},${alpha})`}>
       {#if typeof action === "function"}
-        <input bind:value bind:this={element} type="color" style="visibility: hidden;" {disabled} {...attr} {...events} use:action />
+        <input bind:value={hex} bind:this={element} type="color" style="visibility: hidden;" {disabled} {...attr} {...events} use:action />
       {:else}
-        <input bind:value type="color" style="visibility: hidden;" {disabled} {...attr} {...events} />
+        <input bind:value={hex} type="color" style="visibility: hidden;" {disabled} {...attr} {...events} />
       {/if}
     </div>
   </div>
